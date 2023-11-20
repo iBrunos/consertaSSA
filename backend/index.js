578fc0b2a95e3cfe8a357ce81c8a3d5461654396
Bruno Seixas <102114046+iBrunos@users.noreply.github.com>
2023-11-20 13:35:18 -0300

    a

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import connectDatabase from "./src/database/db.js"
 import dotenv from "dotenv"
 
 import authRoute from "./src/routes/auth.routes.js"
+import companyRoute from "./src/routes/company.routes.js"
 import orderRoute from "./src/routes/order.routes.js"
 import userRoute from "./src/routes/user.routes.js"
 import sexRoute from "./src/routes/sex.routes.js"
@@ -28,7 +29,7 @@ app.use(express.json())
 app.use("/", sexRoute)
 app.use("/order", orderRoute)
 app.use("/user", userRoute)
-
+app.use("/company", companyRoute)
 
 app.use("/auth", authRoute)
 
